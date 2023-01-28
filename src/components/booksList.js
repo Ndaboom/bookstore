82dd373bceba8dd05e4ad3576955fe373f3279bc
ndaboom <ndabosam084@gmail.com>
2023-01-28 17:42:19 +0100

    Books list set default to []

--- src/components/booksList.js
+++ src/components/booksList.js
@@ -1,22 +1,16 @@
-/* eslint-disable react/destructuring-assignment */
-/* eslint-disable react/state-in-constructor */
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from './book';
 
-class BooksList extends React.Component {
-  state = {
-    books: [],
-  };
-
-  render() {
-    return (
-      <div className="container">
-        {this.state.books.map((book) => (
-          <Book key={book.id} book={book} />
-        ))}
-      </div>
-    );
-  }
+function BooksList() {
+  const books = useSelector((state) => state.books) || [];
+  return (
+    <div className="container">
+      {books.map((book) => (
+        <Book key={book.id} book={book} />
+      ))}
+    </div>
+  );
 }
 
 export default BooksList;
--- src/components/category.js
+++ src/components/category.js
@@ -1,6 +1,3 @@
-/* eslint-disable react/destructuring-assignment */
-/* eslint-disable react/prop-types */
-/* eslint-disable react/no-this-in-sfc */
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { checkStatus } from '../redux/categories/categories';
